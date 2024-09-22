900c004ce08bfee2f962944395a7a8ae8e392e29
Gorgai <gorgaiswag@gmail.com>
2024-09-22 21:12:37 +0200

    asd

---
+++ wwwroot/localStorage.js
@@ -0,0 +1,25 @@
+﻿export function setItem(key, value) {
+    localStorage.setItem(key, value);
+}
+
+export function getItem(key) {
+    return localStorage.getItem(key);
+}
+
+export function preventDefaultContextMenu(element) {
+    if (element instanceof HTMLElement) {
+        element.addEventListener("contextmenu", function (e) {
+            e.preventDefault();
+        });
+    } else {
+        console.error("Invalid element passed to preventDefaultContextMenu");
+    }
+}
+
+window.addKeyListener = (dotNetObject) => {
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'r' || event.key === 'R') {
+            dotNetObject.invokeMethodAsync('ResetGame');
+        }
+    });
+};
